04ae0d2b52b591c9ee34424e85e86c3d80ee7d08
cdx <clbustos@gmail.com>
2017-09-19 05:58:32 -0300

    Resuelto bug: no carga decisiones en otras etapas

--- controllers/decision.rb
+++ controllers/decision.rb
@@ -22,7 +22,7 @@ put '/decision/revision/:revision_id/usuario/:usuario_id/canonico_documento/:cd_
     else
       Decision.insert(:revision_sistematica_id => revision_id,
                       :decision => nil,
-                      :usuario_id => usuario_id, :canonico_documento_id => pk, :etapa => 'revision_titulo_resumen', :comentario => value.strip)
+                      :usuario_id => usuario_id, :canonico_documento_id => pk, :etapa => etapa, :comentario => value.strip)
     end
   end
   return 200
@@ -42,7 +42,7 @@ post '/decision/revision/:revision_id/usuario/:usuario_id/canonico_documento/:cd
     else
       Decision.insert(:revision_sistematica_id => revision_id,
                       :decision => decision,
-                      :usuario_id => usuario_id, :canonico_documento_id => cd_id, :etapa => 'revision_titulo_resumen')
+                      :usuario_id => usuario_id, :canonico_documento_id => cd_id, :etapa => etapa)
     end
   end
   revision=Revision_Sistematica[revision_id]
