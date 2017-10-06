d5eea0926f4af2ce9b4e9d1c3f126713c49982a2
cdx <clbustos@gmail.com>
2017-10-06 00:53:18 -0300

    Se actualiza para el tag solo el #id que corresponde. Esto evita recargar el resto

--- public/js/tags_cd_rs.js
+++ public/js/tags_cd_rs.js
@@ -3,23 +3,25 @@ crear_tag=function(url,val,cd_pk,rs_pk) {
         alert("El tag no tiene texto")
     } else {
         $.post(url, {value: val}, function (data) {
-            $("#tags-cd-"+cd_pk+"-rs-"+rs_pk).replaceWith(data);
-            actualizar_tags_cd_rs();
+            var div_id="#tags-cd-"+cd_pk+"-rs-"+rs_pk
+            $(div_id).replaceWith(data);
+            actualizar_tags_cd_rs(div_id);
         }).fail(function () {
             alert("No se pudo crear el tag")
         })
     }
 };
-actualizar_tags_cd_rs=function() {
-    $(".boton_accion_tag_cd_rs").click(function(){
+actualizar_tags_cd_rs=function(div_id=false) {
+    var selector= div_id ? div_id+" .boton_accion_tag_cd_rs" : ".boton_accion_tag_cd_rs";
+    $(selector).click(function(){
         var url=$(this).attr("data-url");
         var cd_pk=$(this).attr("cd-pk");
         var rs_pk=$(this).attr("rs-pk");
         var tag_id=$(this).attr("tag-pk");
         $.post(url, {tag_id:tag_id}, function (data) {
-
-            $("#tags-cd-"+cd_pk+"-rs-"+rs_pk).replaceWith(data);
-            actualizar_tags_cd_rs();
+            var div_id="#tags-cd-"+cd_pk+"-rs-"+rs_pk
+            $(div_id).replaceWith(data);
+            actualizar_tags_cd_rs(div_id);
         }).fail(function () {
             alert("No se pudo realizar la acción en el tag")
         })
@@ -32,6 +34,7 @@ actualizar_tags_cd_rs=function() {
         var rs_pk=$(this).attr("rs-pk");
         var val=$("#tag-cd-"+cd_pk+"-rs-"+rs_pk+"-nuevotag").val().trim();
         crear_tag(url,val, cd_pk,rs_pk);
+        return(false);
     });
 
     $(".nuevo_tag_cd_rs").on('keypress', function(e) {
@@ -41,6 +44,7 @@ actualizar_tags_cd_rs=function() {
             var rs_pk=$(this).attr("rs-pk");
             var val=$(this).val().trim();
             crear_tag(url,val,cd_pk,rs_pk);
+            return(false);
         }
     });
 };
