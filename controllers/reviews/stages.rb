c1dc2fa60c613721fa695940cd5b70c18c9c643a
cdx <clbustos@gmail.com>
2018-02-27 17:21:23 -0300

    Refactored search processor

--- controllers/reviews/stages.rb
+++ controllers/reviews/stages.rb
@@ -228,32 +228,6 @@ get '/review/:id/stage/:etapa/pattern/:patron/resolution/:resolucion' do |id,eta
   redirect back
 end
 
-post '/resolution/review/:id/canonical_document/:cd_id/stage/:etapa/resolution' do |rev_id, cd_id, etapa|
-
-  resolucion=params['resolucion']
-  user_id=params['user_id']
-
-
-  $db.transaction(:rollback=>:reraise) do
-
-    res=Resolucion.where(:revision_sistematica_id=>rev_id, :canonico_documento_id=>cd_id, :etapa=>etapa)
-    if res.empty?
-      Resolucion.insert(:revision_sistematica_id=>rev_id, :canonico_documento_id=>cd_id, :etapa=>etapa, :resolucion=>resolucion, :usuario_id=>user_id, :comentario=>"Resuelto en forma especifica en #{DateTime.now.to_s}")
-    else
-      res.update(:resolucion=>resolucion, :usuario_id=>user_id, :comentario=>"Actualizado en forma especifica en #{DateTime.now.to_s}")
-    end
-  end
-
-  revision=Revision_Sistematica[rev_id]
-  ars=AnalisisRevisionSistematica.new(revision)
-
-  res=Resolucion.where(:revision_sistematica_id=>rev_id, :canonico_documento_id=>cd_id, :etapa=>etapa)
-
-
-  rpc=ars.resolucion_por_cd (etapa)
-
-  partial(:buttons_resolution, :locals=>{:rpc=>rpc, :cd_id=>cd_id.to_i, :etapa=>etapa, :usuario_id=>user_id, :revision=>revision})
-end
 
 
 
