e6ee7e36434ce3fef66e25927d8c91559b3ac861
clbustos <clbustos@gmail.com>
2018-08-20 05:39:52 -0300

    Added require_relative for Buhos::Helpers on FileHandler

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -26,6 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+require_relative "buhos/helpers"
 
 # Namespace for files related UX
 module FileHandler
