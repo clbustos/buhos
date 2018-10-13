87295dcded860c3ce0716eb53e7ca10ca21d7f41
cdx <clbustos@gmail.com>
2018-10-13 03:25:51 -0300

    Better references on apa style

--- spec/reference_methods_spec.rb
+++ spec/reference_methods_spec.rb
@@ -5,16 +5,40 @@ describe 'ReferenceMethods mixin' do
     @fake_cd=Object.new
     @fake_cd.extend ReferenceMethods
   end
-  it ".authors_apa_6 should retrieve correct authors if defined" do
-    def @fake_cd.author; "SN1, FN1 and SN2, FN2 "; end
-    expect(@fake_cd.authors_apa_6).to eq("SN1, FN1, & SN2, FN2")
-  end
-  it ".authors_apa_6 should retrieve '--NA--' if author is nil" do
-    def @fake_cd.author; nil; end
-    expect(@fake_cd.authors_apa_6).to eq("--NA--")
-  end
 
-  it ".cite_apa_6 should retrieve empty data if no information is given" do
+  describe '#authors_apa_6' do
+
+    it "should retrieve correct authors names using commas (n<7)" do
+      def @fake_cd.author; "SN1,    FN1 FN1b and SN2, FN2 "; end
+      expect(@fake_cd.authors_apa_6).to eq("SN1, F.F., & SN2, F.")
+    end
+    it "should retrieve correct authors using only surnames" do
+      def @fake_cd.author; "SN1 and SN2"; end
+      expect(@fake_cd.authors_apa_6).to eq("SN1, & SN2")
+    end
+
+    it "should retrieve correct authors names without using commas (n<7)" do
+      def @fake_cd.author; "FN1 SN1 and FN2 SN2"; end
+      expect(@fake_cd.authors_apa_6).to eq("SN1, F., & SN2, F.")
+    end
+
+    it "should retrieve correct authors names (n>=7)" do
+      def @fake_cd.author; 1.upto(10).map {|v| "SN#{v}, FN#{v}"}.join(" and "); end
+      expect(@fake_cd.authors_apa_6).to eq("SN1, F., SN2, F., SN3, F., SN4, F., SN5, F., ..., SN10, F.")
+    end
+
+
+    it "should retrieve '--NA--' if author is nil" do
+      def @fake_cd.author; nil; end
+      expect(@fake_cd.authors_apa_6).to eq("--NA--")
+    end
+    it "should retrieve '--NA--' if author is ','" do
+      def @fake_cd.author; '  ,  and  ,  '; end
+      expect(@fake_cd.authors_apa_6).to eq("--NA--")
+    end
+
+  end
+  it "#cite_apa_6 should retrieve empty data if no information is given" do
     def @fake_cd.author; nil; end
     def @fake_cd.year; 0; end
     def @fake_cd.title; nil; end
