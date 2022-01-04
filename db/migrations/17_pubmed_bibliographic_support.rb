1b0b39c2a108f91a95ed7fa46d89a475643a812d
clbustos <clbustos@gmail.com>
2022-01-04 16:57:49 -0300

    Se inserta pubmed en bases bibliograficas

--- db/migrations/17_pubmed_bibliographic_support.rb
+++ db/migrations/17_pubmed_bibliographic_support.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     alter_table(:canonical_documents) do
       add_column :pubmed_id, String, :null=>true
     end
-    from(:bibliographic_databases).replace(:name=>"pubmed")
+    run "INSERT INTO bibliographic_databases (name) VALUES ('pubmed') "
 
   end
 
