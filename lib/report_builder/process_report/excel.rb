4f1432d09234d3cdbdf22ae2da8f0c229a66f507
clbustos <clbustos@gmail.com>
2022-03-09 02:42:42 -0300

    Added search stage to excel for process report

--- lib/report_builder/process_report/excel.rb
+++ lib/report_builder/process_report/excel.rb
@@ -33,6 +33,7 @@ module ReportBuilder
   class ProcessReport
     class Excel
       include ::Buhos::StagesMixin
+      include ::Sinatra::I18n::Helpers
       attr_reader :sr
       attr_reader :report
       def self.create(pr)
@@ -49,10 +50,28 @@ module ReportBuilder
         @package = Axlsx::Package.new
         wb = @package.workbook
         @blue_cell = wb.styles.add_style  :fg_color => "0000FF", :sz => 14, :alignment => { :horizontal=> :center }
+        add_stage_search(wb)
         add_stage(wb,:screening_title_abstract)
         add_stage(wb,:screening_references)
         add_stage(wb,:review_full_text)
 
+      end
+
+      def add_stage_search(wb)
+
+        wb.add_worksheet(:name => I18n::t(get_stage_name( :search ))) do |sheet|
+          sheet.add_row     [I18n::t(:Id), I18n::t(:User_name), I18n::t("search.Source"), I18n::t(:Bibliographic_database),
+                             I18n::t(:Date), I18n::t(:Search_criteria), I18n::t(:Description), I18n::t(:File), I18n::t(:Search_valid),
+                             I18n::t(:Count_records)
+                            ],
+                            :style=> [@blue_cell]*10
+          @sr.searches.each do |g|
+            fn=g.filename.nil? ? I18n::t(:No_file) : "#{g.filename} [#{g.filetype}]"
+            sheet.add_row [g.id, User[g[:user_id]][:name], I18n::t(g.source_name), g.bibliographical_database_name,
+                           g.date_creation, g.search_criteria, g.description, fn, t_yes_no_nil(g.valid),
+                          g.records_n]
+          end
+        end
       end
       def add_stage(wb,stage)
         cds           = CanonicalDocument.where(:id=>@sr.cd_id_by_stage(stage)).order(:year,:author)
@@ -60,11 +79,15 @@ module ReportBuilder
         resolutions   = report.ars.resolution_by_cd(stage)
 
         wb.add_worksheet(:name => I18n::t(get_stage_name(stage))) do |sheet|
-          sheet.add_row     [I18n::t(:Canonical_document)]+sr.group_users.map {|user| user[:name]}+[I18n::t(:Resolution)], :style=> [@blue_cell]*(2+sr.group_users.count)
+          sheet.add_row     [I18n::t(:Canonical_document)] +
+                              sr.group_users.map {|user| user[:name]}+[I18n::t(:Resolution)],
+                            :style=> [@blue_cell]*(2+sr.group_users.count)
 
           cds.each do |cd|
-          sheet.add_row [cd.ref_apa_6_brief] + @sr.group_users.map {|user| I18n::t(adu[user[:id]][:adu].decision_cd_id(cd[:id]))} + [I18n::t(resolutions[cd[:id]])]
-          sheet.column_info.first.width = 50
+            sheet.add_row [cd.ref_apa_6_brief] + @sr.group_users.map {
+              |user| I18n::t(adu[user[:id]][:adu].decision_cd_id(cd[:id]))
+            } + [I18n::t(resolutions[cd[:id]])]
+            sheet.column_info.first.width = 50
           end
         end
       end
