8284f5fc00b8e5c9f72163d44747ce07e5e5632a
clbustos <clbustos@gmail.com>
2023-04-18 15:41:04 -0400

    Updated copyright notice to 2016-2023

--- installer.rb
+++ installer.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 #
-# Copyright (c) 2016-2022, Claudio Bustos Navarrete
+# Copyright (c) 2016-2023, Claudio Bustos Navarrete
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
